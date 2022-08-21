e9f978a3f7e9cbd22484e23076be24daddaa23cf
João Ygor Ramalho <82791430+joaoygorr@users.noreply.github.com>
2022-08-21 00:59:53 -0400

    Arquivo css + bootstrap
    importando bootstrap

---
+++ src/Service/Api.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+const BaseUrl = "http://localhost:8080/unionmangas";
+
+export const ApiMangas = axios.create({
+    baseURL: BaseUrl,
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+// Style
+import './Global.css';
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+// React Bootstrap Configuration
+import "../node_modules/react-bootstrap/dist/react-bootstrap";
+import "../node_modules/bootstrap/dist/css/bootstrap.css"
+import "../node_modules/bootstrap/dist/js/bootstrap"
+
+import Home from './Page/Home/index';
+
+const rootElement = document.getElementById("root")!;
+const root = createRoot(rootElement);
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <StrictMode>
+    <Home />
+  </StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
