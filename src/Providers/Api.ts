a245463f9069db4bc4c34cdb0d65f78c7076fa6f
João Ygor <ygorvieira2002@gmail.com>
2022-08-24 18:41:30 -0400

    tratamento de error nos interceptors

--- src/Providers/Api.ts
+++ src/Providers/Api.ts
@@ -1,11 +1,15 @@
-import axios from "axios";
+import axios, { AxiosError } from "axios";
 
 const BaseUrl = "http://localhost:8080/unionmangas";
-
 export const instance = axios.create({ baseURL: BaseUrl });
 
 // Tratamento de resposta ou erro da Api
 instance.interceptors.response.use(async (response) => {
-    console.log("Response no interceptors:", response);
     return response
+}, function (error: AxiosError) {
+    if (error.response) {
+        const handleError = error.response
+        return Promise.reject(handleError);
+    }
+    return Promise.reject(error);
 })
