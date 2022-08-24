403adbd93d8bcc7716130d97b53c1b4bd42f2244
João Ygor <ygorvieira2002@gmail.com>
2022-08-24 18:40:59 -0400

    Requisição para novos mangas

--- src/Service/MangasService.ts
+++ src/Service/MangasService.ts
@@ -1,11 +1,18 @@
 import { instance } from "../Providers";
 
-const getManga = () => instance.get("/mangas", {
+const getMangas = () => instance.get("/mangas", {
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+
+const getMangasWeek = () => instance.get("/mangas/week", {
     headers: {
         "Content-Type": "application/json"
     }
 })
 
 export const MangasService = {
-    getManga
+    getMangas,
+    getMangasWeek
 };
