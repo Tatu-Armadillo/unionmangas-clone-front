e91edd45c6d93566968c701367e1cd586b057e8c
João Ygor <ygorvieira2002@gmail.com>
2022-08-25 18:29:55 -0400

    map dos mangas

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -1,11 +1,14 @@
 import { useEffect, useState } from "react";
+import { Card } from "react-bootstrap";
 // Components
 import Navbar from "../Components/Navbar/Navbar";
 // Service 
-import { MangasService } from "../Service/MangasService";
+import { MangasService } from "../Service";
+// Interface
+import { IManga } from "../Interfaces"
 
 function Home() {
-  const [mangas, setMangas] = useState({})
+  const [mangas, setMangas] = useState<IManga[]>([]);
 
   useEffect(() => {
     MangasService.getMangas()
@@ -13,10 +16,25 @@ function Home() {
         setMangas(response.data)
       })
   }, [])
-  
+
   return (
-    <div className="App">
+    <div>
       <Navbar />
+      <div className="container">
+        {mangas.map((manga, key) => {
+          return (
+            <Card key={key} style={{ width: '18rem' }}>
+              <Card.Img variant="top" src={manga.linkImage} />
+              <Card.Body>
+                <Card.Title>{manga.mainTitle}</Card.Title>
+                <Card.Text>
+                  
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          )
+        })}
+      </div>
     </div>
   );
 }
