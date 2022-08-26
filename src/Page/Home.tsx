6a09199a403b63e92ac6b0c3b428f834c42f697c
João Ygor <ygorvieira2002@gmail.com>
2022-08-26 17:27:01 -0400

    ajustes
    removendo map e chamando componente

--- src/Page/Home.tsx
+++ src/Page/Home.tsx
@@ -2,38 +2,15 @@ import { useEffect, useState } from "react";
 import { Card } from "react-bootstrap";
 // Components
 import Navbar from "../Components/Navbar/Navbar";
-// Service 
-import { MangasService } from "../Service";
-// Interface
-import { IManga } from "../Interfaces"
+import InputMangas from "../Components/InputsMangas/InputMangas";
 
 function Home() {
-  const [mangas, setMangas] = useState<IManga[]>([]);
-
-  useEffect(() => {
-    MangasService.getMangas()
-      .then((response) => {
-        setMangas(response.data)
-      })
-  }, [])
 
   return (
     <div>
       <Navbar />
       <div className="container">
-        {mangas.map((manga, key) => {
-          return (
-            <Card key={key} style={{ width: '18rem' }}>
-              <Card.Img variant="top" src={manga.linkImage} />
-              <Card.Body>
-                <Card.Title>{manga.mainTitle}</Card.Title>
-                <Card.Text>
-                  
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          )
-        })}
+        <InputMangas />        
       </div>
     </div>
   );
